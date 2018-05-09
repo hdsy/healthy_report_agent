a783d53130a4e252d5bfb9b8446302544337ffeb
gaoyong <gaoyong@wucar.com.cn>
2018-05-09 10:31:04 +0800

    修改set的空判断条件

--- CFileProcessingStatus.h
+++ CFileProcessingStatus.h
@@ -256,7 +256,7 @@ public:
 
 		for(iter=m_mapFileProcessingData.begin();iter != m_mapFileProcessingData.end();iter++)
 		{
-			if(std::set<std::string>::end == tSetFileName.find((iter->second)->szFileName))
+			if(tSetFileName.end() == tSetFileName.find((iter->second)->szFileName))
 				RemoveFile(iter->second);
 		}
 
